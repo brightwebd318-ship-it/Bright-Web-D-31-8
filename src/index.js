7d26d0cc0bca48f0fdc4a80c7ad51e40b3dcd501
shanshajis <shanshaji4477@gmail.com>
2026-03-17 11:34:23 +0530

    Implement perfect SSR/SSG via react-snap and BrowserRouter migration

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import { createRoot, hydrateRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const rootElement = (
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+if (container.hasChildNodes()) {
+  hydrateRoot(container, rootElement);
+} else {
+  createRoot(container).render(rootElement);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
